06aeb0d84e48c23b417cbca66c8b01fbb6431e0f
Amadeus Alerta <efe3365@gmail.com>
2023-10-16 11:15:01 +0300

    Nacho

--- Ders/Omid/16_10_23_02/Form1.cs
+++ Ders/Omid/16_10_23_02/Form1.cs
@@ -10,8 +10,7 @@ namespace _16_10_23_02
         int degree1, degree2;
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            degree1=Convert.ToInt32(textBox1.Text);
-            textBox1.Text = degree1.ToString();
+
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
@@ -21,13 +20,19 @@ namespace _16_10_23_02
 
         private void button1_Click(object sender, EventArgs e)
         {
-            degree2=Convert.ToInt32((degree1*1.8)+32);
+            degree1 = Convert.ToInt32(textBox1.Text);
+            textBox1.Text = degree1.ToString();
+
+            degree2 =Convert.ToInt32((degree1*1.8)+32);
             textBox2.Text = degree2.ToString();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            degree2 = Convert.ToInt32((degree1 -32) /1.8);
+            degree1 = Convert.ToInt32(textBox1.Text);
+            textBox1.Text = degree1.ToString();
+
+            degree2 = Convert.ToInt32((degree1 -32));
             textBox2.Text = degree2.ToString();
         }
     }
