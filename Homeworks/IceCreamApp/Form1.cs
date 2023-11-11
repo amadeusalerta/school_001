9022a1af96e0a6282b58b78af3add906002c83cd
Amadeus Alerta <efe3365@gmail.com>
2023-11-11 18:16:06 +0300

    Update

--- Homeworks/IceCreamApp/Form1.cs
+++ Homeworks/IceCreamApp/Form1.cs
@@ -23,7 +23,8 @@ namespace IceCreamApp
         public double total_price=0;
         public double current_price=0;
         public double total_calories=0;
-        public double current_calories = 0;
+
+        // L/B/S/C
 
         public int[] calories_of_diets = { 115,175,135,235};
         public int[] calories_of_low_fats = { 125, 325, 225, 340 };
@@ -32,6 +33,20 @@ namespace IceCreamApp
         public double[] price_of_diets = { 0.25,0.55,0.75,0.80};
         public double[] price_of_low_fats = { 0.35, 0.65,0.85,0.90};
         public double[] price_of_fats = { 0.40,0.80,0.85,1 };
+
+        public int piece_of_lemon;
+        public int piece_of_banana;
+        public int piece_of_straw;
+        public int piece_of_choco;
+
+        public double lemon_price;
+        public double lemon_calories;
+        public double banana_price;
+        public double banana_calories;
+        public double straw_price;
+        public double straw_calories;
+        public double choco_price;
+        public double choco_calories;
 
 
 
@@ -112,6 +127,7 @@ namespace IceCreamApp
         {
             if (radioButton1.Checked)
             {
+
                 groupBox3.Enabled =true;
             }
         }
@@ -134,27 +150,18 @@ namespace IceCreamApp
 
         private void radioButton4_CheckedChanged(object sender, EventArgs e)
         {
-            if (radioButton4.Checked)
-            {
-                current_calories = current_calories + peanut_kcal;
-            }
-        }
 
+        }
         private void radioButton5_CheckedChanged(object sender, EventArgs e)
         {
-            if (radioButton4.Checked)
-            {
-                current_calories = current_calories + hazenut_kcal;
-            }
+
         }
 
         private void radioButton6_CheckedChanged(object sender, EventArgs e)
         {
-            if (radioButton4.Checked)
-            {
-                current_calories = current_calories + anteplnut_kcal;
-            }
+
         }
+
 
         private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -224,13 +231,93 @@ namespace IceCreamApp
 
         private void textBox4_TextChanged(object sender, EventArgs e)
         {
-            current_calories = Convert.ToDouble(textBox3.Text);
-            textBox3.Text = current_calories.ToString();
+
         }
 
         private void textBox6_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void MainManager(object sender, EventArgs e)
+        {
+
+            if (radioButton1.Checked)
+            {
+                lemon_calories = calories_of_diets[0];
+                straw_calories = calories_of_diets[1];
+                banana_calories = calories_of_diets[2];
+                choco_calories = calories_of_diets[3];
+
+                lemon_price = price_of_diets[0];
+                straw_price = price_of_diets[1];
+                banana_price = price_of_diets[2];
+                choco_price = price_of_diets[3];
+
+                piece_of_lemon =(int)numericUpDown1.Value;
+                piece_of_banana = (int)numericUpDown2.Value;
+                piece_of_straw = (int)numericUpDown3.Value;
+                piece_of_choco = (int)numericUpDown4.Value;
+            }
+
+            if (radioButton2.Checked)
+            {
+                lemon_calories = calories_of_low_fats[0];
+                straw_calories = calories_of_low_fats[1];
+                banana_calories = calories_of_low_fats[2];
+                choco_calories = calories_of_low_fats[3];
+
+                lemon_price = price_of_low_fats[0];
+                straw_price = price_of_low_fats[1];
+                banana_price = price_of_low_fats[2];
+                choco_price = price_of_low_fats[3];
+
+                piece_of_lemon = (int)numericUpDown1.Value;
+                piece_of_banana = (int)numericUpDown2.Value;
+                piece_of_straw = (int)numericUpDown3.Value;
+                piece_of_choco = (int)numericUpDown4.Value;
+
+
+            }
+
+            if (radioButton3.Checked)
+            {
+                lemon_calories = calories_of_fats[0];
+                straw_calories = calories_of_fats[1];
+                banana_calories = calories_of_fats[2];
+                choco_calories = calories_of_fats[3];
+
+                lemon_price = price_of_fats[0];
+                straw_price = price_of_fats[1];
+                banana_price = price_of_fats[2];
+                choco_price = price_of_fats[3];
+
+                piece_of_lemon = (int)numericUpDown1.Value;
+                piece_of_banana = (int)numericUpDown2.Value;
+                piece_of_straw = (int)numericUpDown3.Value;
+                piece_of_choco = (int)numericUpDown4.Value;
+            }
+
+            total_calories = (piece_of_lemon * lemon_calories) + (piece_of_banana *banana_calories ) + (piece_of_straw *straw_calories ) + (piece_of_choco * choco_calories);
+            total_price=(piece_of_lemon*lemon_price)+(piece_of_banana*banana_price)+(piece_of_straw*straw_price)+(piece_of_choco*choco_price);
+
+            if (radioButton4.Checked)
+            {
+                total_calories = total_calories + (peanut_kcal*piece_of_banana) + (peanut_kcal * piece_of_lemon) + (peanut_kcal * piece_of_straw) + (peanut_kcal * piece_of_choco);
+            }
+
+            if (radioButton4.Checked)
+            {
+                total_calories = total_calories + (hazenut_kcal * piece_of_banana) + (hazenut_kcal * piece_of_lemon) + (hazenut_kcal * piece_of_straw) + (hazenut_kcal * piece_of_choco);
+            }
+
+            if (radioButton4.Checked)
+            {
+                total_calories = total_calories + (anteplnut_kcal * piece_of_banana) + (anteplnut_kcal * piece_of_lemon) + (anteplnut_kcal * piece_of_straw) + (anteplnut_kcal * piece_of_choco);
+            }
+
+            textBox3.Text = total_price.ToString();
+            textBox4.Text=total_calories.ToString();
         }
 
     }
