49092c194f2b64360c7d840eb767458e288334b0
Amadeus Alerta <efe3365@gmail.com>
2023-12-11 11:15:04 +0300

    Sheqwes

---
+++ Ders/Omid/11_12_2023/11_12_2023/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _11_12_2023
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Car MyCar=new Car();
+            string result = MyCar.honk();
+            textBox1.Text = result;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MyClass1 obj1 = new MyClass1();
+
+            obj1.MyProperty = 10;
+
+            int value = obj1.MyProperty;
+            textBox3.Text=value.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MyClassRoom MyGrade = new MyClassRoom();
+            int grade = MyGrade.grade();
+            textBox2.Text = grade.ToString();
+        }
+    }
+}
---
+++ Ders/Omid/11_12_2023/11_12_2023/Vehicle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_12_2023
+{
+    public class Vehicle
+    {
+        public string brand_01 = "Ford";
+        public string honk()
+        {
+            return "Karadenizlileri Sevmiyorum!";
+        }
+
+
+    }
+
+
+    public class Car : Vehicle
+    {
+        public string name_01 = "Mustang";
+    }
+
+    public class MyClassRoom
+    {
+        private int MyGrade;
+
+        public int grade()
+        {
+            return 20;
+        }
+
+    }
+
+    public class MyClass1
+    {
+        private int myField;
+
+        public int MyProperty
+        { get { return myField; } set {  myField = value; } }   
+
+
+    }
+
+}
